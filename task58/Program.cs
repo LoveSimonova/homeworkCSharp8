e6883e5cfd31317ae5c1d9e4668699b26773d45c
«SimLove» <simonolyuba@yandex.ru>
2023-06-20 21:06:35 +0300

    add task58

--- task58/Program.cs
+++ task58/Program.cs
@@ -1,16 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int GetRows()
 {
-    Console.WriteLine("Введите количество строк: ");
+    Console.WriteLine("Введите количество строк первой матрицы: ");
     return Convert.ToInt32(Console.ReadLine());
 }
 
 int GetColumns()
 {
-    Console.WriteLine("Введите количество столбцов: ");
+    Console.WriteLine("Введите количество столбцов первой матрицы: ");
     return Convert.ToInt32(Console.ReadLine());
 }
 
+int GetRowSecond()
+{
+    Console.WriteLine("Введите количество строк второй матрицы: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
 double[,] GetMass(int rows, int columns)
 {
     double[,] massive = new double[rows,columns];
@@ -30,13 +35,18 @@ double[,] GetMass(int rows, int columns)
 double[,]  GetProizv(double[,] a,double[,] b)
 {
     double[,] masRez=new double  [a.GetLength(0),b.GetLength(1)];
-    
+    int count=0;
     for (int i=0;i<a.GetLength(0);i++)
     {
         
-    for(int j=0;j<b.GetLength(1);j++)
-       {
-        masRez[i,j]+=(a[i,j]*b[j,i]);
+        for(int j=0;j<b.GetLength(1);j++)
+        {
+            while(count<b.GetLength(1))
+            {
+                masRez[i,j]+=a[i,count]*b[count,j];
+                count++;
+            }  
+            count=0;      
         }
     }
     return masRez;
@@ -53,6 +63,7 @@ Console.WriteLine() ;
 }
 int m=GetRows();
 int n=GetColumns();
+int k = GetRowSecond();
 double[, ] a=GetMass(m,n);
-double [,] b=GetMass(n,m);
+double [,] b=GetMass(k,m);
 PrintArray(GetProizv(a,b));
